e0cb580701a477107ff5a35a57fe127bc7415f5e
mepkn <iamthepkn@gmail.com>
2020-04-03 16:20:02 +0530

    Added selection

--- ytdlPrg.bat
+++ ytdlPrg.bat
@@ -1,2 +1,17 @@
 @ECHO OFF
 ECHO ----------------------------------------------------------------------------------------------------------------------
+:selection
+ECHO 1 Video Download
+ECHO 2 Audio Download
+ECHO 3 Video Download via Text File
+ECHO 4 Audio Download via Text File
+ECHO 5 Update
+ECHO.
+SET /P OPTION="[Enter option ] "
+if %option% == 1 (goto videodownload)
+if %option% == 2 (goto audiodownload)
+if %option% == 3 (goto videodownloadtxt)
+if %option% == 4 (goto audiodownloadtxt)
+if %option% == 5 (goto update)
+ECHO Unknown value 
+goto selection
